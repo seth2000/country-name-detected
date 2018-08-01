0466009e7aef406b5c0fcf6a6831126a1efdfaef
Seth Feng Li <sethfengli@gmail.com>
2018-08-01 17:01:09 +1000

    clear the project
    remove the wrong initial

--- NLP_ALL/text_classifier/textblob/readMe.txt
+++
@@ -1,3 +0,0 @@
-有监督的分类，训练男女姓名文件对男女名字进行分类
-需要先安装 textblob
-pip install -U textblob
--- NLP_ALL/word_tagging/jieba/word_tagging.py
+++
@@ -1,5 +0,0 @@
-#encoding=utf-8
-import jieba.posseg as pseg
-words = pseg.cut("我爱北京天安门")
-for word, flag in words:
-    print('%s %s' % (word, flag))
